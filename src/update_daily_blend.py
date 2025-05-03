37627e3c61f7ce09ebe6564c40432fd0ea7fa88d
Aaron Mamparo <aaronmamparo@gmail.com>
2025-05-02 19:26:17 -0500

    update algorithm

--- src/update_daily_blend.py
+++ src/update_daily_blend.py
@@ -13,27 +13,40 @@ def main(environment: Environment) -> None:
     session.token_refresh(environment.get('TIDAL_REFRESH_TOKEN'))
     session.load_oauth_session('Bearer', session.access_token)
 
-    playlist_tracks = session.mix(environment.get('NEW_ARRIVALS_MIX_ID')).items()
-
-    for track in session.mix(environment.get('DAILY_DISCOVER_MIX_ID')).items():
-        if track.id in [x.id for x in playlist_tracks]:
-            continue
-        playlist_tracks.append(track)
+    daily_discover_tracks = session.mix(environment.get('DAILY_DISCOVER_MIX_ID')).items()
 
     already_seen_artist_ids = []
-    for t in playlist_tracks:
+    for t in daily_discover_tracks:
         already_seen_artist_ids.extend([a.id for a in t.artists])
+
+    additional_tracks = []
+    for track in session.mix(environment.get('NEW_ARRIVALS_MIX_ID')).items():
+        if track.id in [x.id for x in daily_discover_tracks]:
+            continue
+        track_artist_ids = [a.id for a in track.artists]
+        if any(x in already_seen_artist_ids for x in track_artist_ids):
+            continue
+        additional_tracks.append(track)
+        already_seen_artist_ids.extend(track_artist_ids)
+
+    additional_tracks_target_size = len(additional_tracks) * 2
 
     my_most_listened_tracks = session.mix(environment.get('MY_MOST_LISTENED_MIX_ID')).items()
     shuffle(my_most_listened_tracks)
     for track in my_most_listened_tracks:
-        if len(playlist_tracks) == int(environment.get('DAILY_BLEND_SIZE')):
+        if len(additional_tracks) == additional_tracks_target_size:
             break
         track_artist_ids = [a.id for a in track.artists]
         if any(x in already_seen_artist_ids for x in track_artist_ids):
             continue
-        playlist_tracks.append(track)
+        additional_tracks.append(track)
         already_seen_artist_ids.extend(track_artist_ids)
+
+    shuffle(additional_tracks)
+
+    n_tracks_to_add_from_additional = int(environment.get('DAILY_BLEND_SIZE')) - len(daily_discover_tracks)
+
+    playlist_tracks = daily_discover_tracks + additional_tracks[:n_tracks_to_add_from_additional]
 
     shuffle(playlist_tracks)
     daily_blend = session.playlist(environment.get('DAILY_BLEND_PLAYLIST_ID'))
