41514ed571d6634e8e06f7fa94675faf73b997c2
Aaron Mamparo <aaronmamparo@gmail.com>
2025-08-17 16:35:15 -0500

    improve freshness

--- src/update_daily_blend.py
+++ src/update_daily_blend.py
@@ -1,5 +1,5 @@
-from random import shuffle
-from typing import Optional, List
+import random
+from typing import Optional, List, Set
 
 import requests
 from injector import inject, Injector
@@ -9,7 +9,7 @@ from unidecode import unidecode
 
 from src.environment import Environment
 
-max_track_duration_minutes = 20
+daily_blend_size = 100
 
 
 @inject
@@ -18,51 +18,61 @@ def main(environment: Environment) -> None:
     session.token_refresh(environment.get('TIDAL_REFRESH_TOKEN'))
     session.load_oauth_session('Bearer', session.access_token)
 
-    last_fm_recommendations = get_lastfm_recommendations(session)
-    last_fm_mix = get_lastfm_mix(session)
-    last_fm_library = get_lastfm_library(session)
-
-    daily_discover_tracks = session.mix(environment.get('DAILY_DISCOVER_MIX_ID')).items()
-    new_arrivals_tracks = session.mix(environment.get('NEW_ARRIVALS_MIX_ID')).items()
-
-    daily_blend_tracks = (daily_discover_tracks + new_arrivals_tracks + last_fm_recommendations + last_fm_mix +
-                          last_fm_library)
-    daily_blend_tracks = list({(x.id, x): x for x in daily_blend_tracks}.values())
-    shuffle(daily_blend_tracks)
-    daily_blend_tracks = daily_blend_tracks[:100]
     daily_blend = session.playlist(environment.get('DAILY_BLEND_PLAYLIST_ID'))
+    existing_daily_blend_track_ids = {x.id for x in daily_blend.items()}
+
+    new_daily_blend_track_ids = {x.id for x in session.mix(environment.get('DAILY_DISCOVER_MIX_ID')).items()}
+
+    last_fm_recommendations = get_lastfm_playlist_tidal_track_ids(session, 'recommended')
+    last_fm_mix = get_lastfm_playlist_tidal_track_ids(session, 'mix')
+    last_fm_library = get_lastfm_playlist_tidal_track_ids(session, 'library')
+    new_arrivals = {x.id for x in session.mix(environment.get('NEW_ARRIVALS_MIX_ID')).items()}
+
+    new_daily_blend_track_ids |= (
+                                         last_fm_recommendations | last_fm_mix | last_fm_library | new_arrivals
+                                 ) - existing_daily_blend_track_ids
+
+    if len(new_daily_blend_track_ids) < daily_blend_size:
+        n_needed = daily_blend_size - len(new_daily_blend_track_ids)
+        extras = list(new_arrivals - new_daily_blend_track_ids)
+        random.shuffle(extras)
+        extras = extras[:n_needed]
+        print(f'Adding {len(extras)} duplicates from previous tracklist to fill out daily blend')
+        new_daily_blend_track_ids |= set(extras)
+
     daily_blend.clear()
-    daily_blend.add([str(x.id) for x in daily_blend_tracks])
+    daily_blend.add([str(x) for x in random.sample(list(new_daily_blend_track_ids), daily_blend_size)])
 
 
-def get_lastfm_library(session: Session) -> List[Track]:
-    return get_lastfm_playlist(session, 'https://www.last.fm/player/station/user/amamparo/library?page=1&ajax=1')
-
-def get_lastfm_recommendations(session: Session) -> List[Track]:
-    return get_lastfm_playlist(session, 'https://www.last.fm/player/station/user/amamparo/recommended?page=1&ajax=1')
-
-def get_lastfm_mix(session: Session) -> List[Track]:
-    return get_lastfm_playlist(session, 'https://www.last.fm/player/station/user/amamparo/mix?page=1&ajax=1')
-
-def get_lastfm_playlist(session: Session, url: str) -> List[Track]:
-    tracks = []
-    playlist = requests.get(url).json()['playlist']
+def get_lastfm_playlist_tidal_track_ids(session: Session, _type: str) -> Set[int]:
+    tracks = set()
+    playlist = requests.get(
+        f'https://www.last.fm/player/station/user/amamparo/{_type}?page=1&ajax=1'
+    ).json()['playlist']
     for x in tqdm(playlist):
         track = get_tidal_track(session, x['name'], [a['name'] for a in x['artists']])
         if track:
-            tracks.append(track)
+            tracks.add(track.id)
     return tracks
 
 
 def get_tidal_track(session: Session, last_fm_title: str, last_fm_artists: List[str]) -> Optional[Track]:
     search_results = session.search(' '.join(last_fm_artists) + ' ' + last_fm_title, models=[Track])['tracks']
     for search_result in search_results:
-        if unidecode(last_fm_title.lower()) not in unidecode(search_result.name.lower()):
+        if norm(last_fm_title) not in norm(search_result.name):
             continue
-        if any(x for x in last_fm_artists if unidecode(x.lower()) not in [unidecode(a.name.lower()) for a in search_result.artists]):
+
+        last_fm_artists = {norm(x) for x in last_fm_artists}
+        tidal_artists = {norm(a.name) for a in search_result.artists}
+        if not last_fm_artists <= tidal_artists:
             continue
+
         return search_result
     return None
+
+
+def norm(text: str) -> str:
+    return unidecode(text).lower().strip()
 
 
 # pylint: disable=unused-argument
