837d2e4a300a8dc013a19c59732b9a61d3992836
Aaron Mamparo <aaronmamparo@gmail.com>
2025-08-17 20:23:02 -0500

    color printing

--- src/update_daily_blend.py
+++ src/update_daily_blend.py
@@ -18,12 +18,12 @@ def main(environment: Environment, tidal: Tidal, last_fm: LastFm) -> None:
         last_fm_tracks = last_fm.get_mix(mix_type)
         with tqdm(total=len(last_fm_tracks), desc=f'Scanning last.fm {mix_type}') as progress:
             for last_fm_track in last_fm_tracks:
-                progress.write(str(last_fm_track))
                 tidal_track = tidal.find_equivalent_track(last_fm_track)
                 if tidal_track:
+                    progress.write(f'\033[92m✓ {last_fm_track}\033[0m')
                     daily_blend_track_ids.append(str(tidal_track.id))
                 else:
-                    progress.write(f'Failed to find tidal equivalent for: {last_fm_track}')
+                    progress.write(f'\033[91m✗ Failed to find: {last_fm_track}\033[0m')
                 progress.update(1)
 
     shuffle(daily_blend_track_ids)
