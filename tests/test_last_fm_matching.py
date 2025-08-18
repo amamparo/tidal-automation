3c6b0b440ef41e85a45d1cb59831d48d4425b410
Aaron Mamparo <aaronmamparo@gmail.com>
2025-08-17 19:40:48 -0500

    add failing test

--- tests/test_last_fm_matching.py
+++ tests/test_last_fm_matching.py
@@ -68,6 +68,15 @@ class LastFmMatching(TestCase):
         self.assertEqual(track.name, "I Could Fall In Love")
         self.is_in_artists("Selena", track)
 
+    def test_matt_and_kim(self):
+        track = self.tidal.find_equivalent_track(LastFmTrack(
+            'Daylight',
+            {'Matt & Kim'}
+        ))
+
+        self.assertEqual(track.name, 'Daylight')
+        self.is_in_artists("Matt & Kim", track)
+
 
     def is_in_artists(self, artist: str, tidal_track: Track):
         tidal_artists = {artist.name.lower() for artist in tidal_track.artists}
